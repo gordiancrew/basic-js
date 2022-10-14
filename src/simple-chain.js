46881cbef956a533e5982e3d13c80c7c297a3331
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-10-15 02:06:14 +0300

    fix: solution chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,36 +4,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
-  constructor() {
-    this.arr = new Array();
-
-  },
+ const chainMaker = {
+    
+  arr:new Array(),
 
   getLength() {
     return arr.length;
   },
   addLink(value) {
+  
     this.arr.push(value);
+    return this;
   },
   removeLink(position) {
-    this.arr.splice(position,1);
 
+    if(typeof position !="number"||position>this.arr.length||position<1){
+      this.arr=new Array();
+     throw new Error( 'You can\'t remove incorrect link!');
+    
+      
+    }
+    this.arr.splice(position-1,1);
+    return this;
   },
   reverseChain() {
     this.arr.reverse();
+    return this;
   },
   finishChain() {
     let res = "";
     for (let i = 0; i < this.arr.length; i++) {
-      if (i === arr[arr.length - 1]) {
-        res = res + "( " + arr[i] + " )";
+      if (i === this.arr.length - 1) {
+        res = res + "( " + this.arr[i] + " )";
       }
       else {
-        res = res + "( " + arr[i] + " )~~";
+        res = res + "( " + this.arr[i] + " )~~";
       }
 
     }
+   this.arr=new Array();
     return res;
   }
 };
