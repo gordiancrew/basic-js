b6b044b5953bca8aba5435b0a171149d9c2a8b08
Uladzimir Kazantsau <gordiancrew@gmail.com>
2022-10-17 14:25:07 +0300

    add: solution virg-cript

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,90 +20,113 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-
-
-  encrypt() {
-
-    if(arguments.length!==2||arguments[0]===undefined||arguments[1]===undefined){
-      throw new Error("Incorrect arguments!");
+  constructor() {
+    if (arguments[0] === false) {
+      this.arg = false;
+    } else {
+      this.arg = true;
     }
-    let arrEnterLetters = arguments[0].split("");
-    let arrCodeWord = arguments[1].split("");
-    let arrCodeWordLength = [];
-    let arrNumbersResult = [];
-    let arrCharResult = [];
+  }
 
-    let codeWordLength = arrCodeWord.length;
-    for (let i = 0; i < arrEnterLetters.length; i++) {
-      arrCodeWordLength.push(arrCodeWord[i < codeWordLength ? i : i % codeWordLength].toUpperCase());
+
+
+
+
+
+encrypt() {
+
+  if (arguments.length !== 2 || arguments[0] === undefined || arguments[1] === undefined) {
+    throw new Error("Incorrect arguments!");
+  }
+  let arrEnterLetters = arguments[0].split("");
+  let arrCodeWord = arguments[1].split("");
+  let arrCodeWordLength = [];
+  let arrNumbersResult = [];
+  let arrCharResult = [];
+
+  let codeWordLength = arrCodeWord.length;
+  for (let i = 0; i < arrEnterLetters.length; i++) {
+    arrCodeWordLength.push(arrCodeWord[i < codeWordLength ? i : i % codeWordLength].toUpperCase());
+  }
+
+  let cryptGapCount = 0;
+  for (let t = 0; t < arrEnterLetters.length; t++) {
+    let parseNumber;
+
+    if ((arrEnterLetters[t].toUpperCase().charCodeAt(0) >= 65) && (arrEnterLetters[t].toUpperCase().charCodeAt(0) <= 90)) {
+      parseNumber = arrEnterLetters[t].toUpperCase().charCodeAt(0) + (arrCodeWordLength[cryptGapCount++].charCodeAt(0) - 65);
+      parseNumber = parseNumber <= 90 ? parseNumber : (64 + (parseNumber - 90));
+
+    } else {
+      parseNumber = arrEnterLetters[t];
     }
+    arrNumbersResult.push(parseNumber);
+  }
 
-    let cryptGapCount = 0;
-    for (let t = 0; t < arrEnterLetters.length; t++) {
-      let parseNumber;
-
-      if ((arrEnterLetters[t].toUpperCase().charCodeAt(0) >= 65) && (arrEnterLetters[t].toUpperCase().charCodeAt(0) <= 90)) {
-        parseNumber = arrEnterLetters[t].toUpperCase().charCodeAt(0) + (arrCodeWordLength[cryptGapCount++].charCodeAt(0) - 65);
-        parseNumber = parseNumber <= 90 ? parseNumber : (64 + (parseNumber - 90));
-
-      } else {
-        parseNumber = arrEnterLetters[t];
-      }
-      arrNumbersResult.push(parseNumber);
+  for (let y = 0; y < arrEnterLetters.length; y++) {
+    if (typeof arrNumbersResult[y] !== "number") {
+      arrCharResult.push(arrNumbersResult[y]);
+    } else {
+      let charRes = String.fromCharCode(arrNumbersResult[y]);
+      arrCharResult.push(charRes);
     }
+  }
+  if(this.arg==true){
+  return arrCharResult.join("");
+  }else{
 
-    for (let y = 0; y < arrEnterLetters.length; y++) {
-      if (typeof arrNumbersResult[y] !== "number") {
-        arrCharResult.push(arrNumbersResult[y]);
-      } else {
-        let charRes = String.fromCharCode(arrNumbersResult[y]);
-        arrCharResult.push(charRes);
-      }
-    }
+    arrCharResult.reverse();
     return arrCharResult.join("");
   }
-  decrypt() {
-    if(arguments.length!==2||arguments[0]===undefined||arguments[1]===undefined){
-      throw new Error("Incorrect arguments!");
-    }
-    let arrEnterLetters = arguments[0].split("");
-    let arrCodeWord = arguments[1].split("");
-    let arrCodeWordLength = [];
-    let arrNumbersResult = [];
-    let arrCharResult = [];
-
-    let codeWordLength = arrCodeWord.length;
-    for (let i = 0; i < arrEnterLetters.length; i++) {
-      arrCodeWordLength.push(arrCodeWord[i < codeWordLength ? i : i % codeWordLength].toUpperCase());
-    }
-
-    let cryptGapCount = 0;
-    for (let t = 0; t < arrEnterLetters.length; t++) {
-      let parseNumber;
-
-      if ((arrEnterLetters[t].toUpperCase().charCodeAt(0) >= 65) && (arrEnterLetters[t].toUpperCase().charCodeAt(0) <= 90)) {
-        parseNumber = arrEnterLetters[t].toUpperCase().charCodeAt(0) - (arrCodeWordLength[cryptGapCount++].charCodeAt(0) - 65);
-        parseNumber = parseNumber >=65  ? parseNumber : (90 -(64-parseNumber));
-          
-          
-          
-
-      } else {
-        parseNumber = arrEnterLetters[t];
-      }
-      arrNumbersResult.push(parseNumber);
-    }
-
-    for (let y = 0; y < arrEnterLetters.length; y++) {
-      if (typeof arrNumbersResult[y] !== "number") {
-        arrCharResult.push(arrNumbersResult[y]);
-      } else {
-        let charRes = String.fromCharCode(arrNumbersResult[y]);
-        arrCharResult.push(charRes);
-      }
-    }
-    return arrCharResult.join("");
+}
+decrypt() {
+  if (arguments.length !== 2 || arguments[0] === undefined || arguments[1] === undefined) {
+    throw new Error("Incorrect arguments!");
   }
+  let arrEnterLetters = arguments[0].split("");
+  let arrCodeWord = arguments[1].split("");
+  let arrCodeWordLength = [];
+  let arrNumbersResult = [];
+  let arrCharResult = [];
+
+  let codeWordLength = arrCodeWord.length;
+  for (let i = 0; i < arrEnterLetters.length; i++) {
+    arrCodeWordLength.push(arrCodeWord[i < codeWordLength ? i : i % codeWordLength].toUpperCase());
+  }
+
+  let cryptGapCount = 0;
+  for (let t = 0; t < arrEnterLetters.length; t++) {
+    let parseNumber;
+
+    if ((arrEnterLetters[t].toUpperCase().charCodeAt(0) >= 65) && (arrEnterLetters[t].toUpperCase().charCodeAt(0) <= 90)) {
+      parseNumber = arrEnterLetters[t].toUpperCase().charCodeAt(0) - (arrCodeWordLength[cryptGapCount++].charCodeAt(0) - 65);
+      parseNumber = parseNumber >= 65 ? parseNumber : (90 - (64 - parseNumber));
+
+
+
+
+    } else {
+      parseNumber = arrEnterLetters[t];
+    }
+    arrNumbersResult.push(parseNumber);
+  }
+
+  for (let y = 0; y < arrEnterLetters.length; y++) {
+    if (typeof arrNumbersResult[y] !== "number") {
+      arrCharResult.push(arrNumbersResult[y]);
+    } else {
+      let charRes = String.fromCharCode(arrNumbersResult[y]);
+      arrCharResult.push(charRes);
+    }
+  }
+  if(this.arg==true){
+    return arrCharResult.join("");
+    }else{
+  
+      arrCharResult.reverse();
+      return arrCharResult.join("");
+    }
+}
 }
 
 module.exports = {
